2921278640d0a3c408f438f824b13de411b5fc46
MstrJL300 <joslum3@icloud.com>
2015-11-11 11:28:00 -0500

    Se agrego el boton de eliminar y la función Crear

--- src/main/resources/static/js/app/controller/blogctrl.js
+++ src/main/resources/static/js/app/controller/blogctrl.js
@@ -4,6 +4,7 @@ app.controller("blogCtrl", function($scope,$log,$http) {
     $scope.entry = {title : "Title",
                     content : "Content"};
     $scope.entries = [];
+    
     $log.debug('se creo el $scope');
 
      $scope.loadData = function() {
@@ -16,5 +17,18 @@ app.controller("blogCtrl", function($scope,$log,$http) {
             alert("Ha fallado la petición. Estado HTTP:"+status);
         });
     };
+    
+    $scope.processForm = function() {
+        $log.debug($scope.entry);
+        $http({
+            method  : 'POST',
+            url     : 'blog',
+            data    : $scope.entry
+        }).success(function(data) {
+            console.log(data);
+            $scope.loadData();
+        });
+    };
+    
     $scope.loadData();
 });
